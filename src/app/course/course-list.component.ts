0b3634454acd68b0802d7675968617750979cfca
Wilson Campos <wcampos5@ford.com>
2021-08-15 00:12:46 -0300

    Using get & set to receive filter by field info and update real-time html table

--- src/app/course/course-list.component.ts
+++ src/app/course/course-list.component.ts
@@ -10,7 +10,12 @@ import { CourseService } from "./course.service";
 //Class implements OnInit that requires ngOnInit(): void implementation to start any requirements of this class
 export class CourseListComponent implements OnInit {
     //Members class
-    courses: Course[] = []; //Members
+    _courses: Course[] = [];
+    _filterBy!: string; //used by filter() getter & setter
+    filteredCourses: Course[] = []; //To receive real time typping courses from course-list.component.html
+
+
+
 
     /**INJEÇÃO DE DEPENDENCIA OCORRO ATRAVES DO CONSTRUTOR 
      * Neste caso estamos injectando a classe CourseServices para 
@@ -20,8 +25,38 @@ export class CourseListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        //Igualar o array de cursos chamando o metodo retriveAll() da classe injetada
-        this.courses = this.courseService.retrieveAll();
+        //popula o array de cursos chamando o metodo retriveAll() da classe injetada
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses //because initially there's nothing filtered
+
     }
+
+    /** Getter and Setters 
+     * get used as output even
+     * set used as input even
+    **/
+    public get filter() : string {
+        return this._filterBy;
+    }
+
+    
+    public set filter(value : string) {
+        this._filterBy = value;
+
+        /** Where MAGIC happend - Na atualização da lista de cursos filtrados
+         * 
+         * = this._courses.filter((course: Course)) ==>percorre todos os cursos, onde
+         * => course.name.to.LocaleLowerCase() - converte o curso atual para minuscula
+         * .indexOf(this._filteredBy.toLocateLowerCase()) - Converte a string que esta sendo recebi para minuscula, compara e retorna o indice
+         * > -1 - Retorna o curso caso exita
+         **/
+        //this.filteredCousers = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+        this.filteredCourses = this._courses.filter((course: Course) =>course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+    }
+    
+    
+
+     
+    
 
 }
