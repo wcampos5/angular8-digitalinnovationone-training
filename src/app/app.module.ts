fdbfc031ba0949c8e03c9d2b4ba4425811df9937
Wilson Campos <wcampos5@ford.com>
2021-08-20 09:42:31 -0300

    Created new module and assigned to it the correct responsabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './course/course-list.component';
-import { StarComponent } from './course/star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './Error/error-404.component';
-import { CourseInfoComponent } from './course/course-info.component';
+import { CourseModule } from './course/course.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
-
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-       //Linka a rota root ao component CourseListComponent
-       {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        // "/:id" informa ao angular que estamos passando um paramentro
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       //Rota padrão (root da aplicação)
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
-     
       //Rota padrao (link nao existente)
       {
         path: '**', component: Error404Component
-      },
+      }
       
     ])
   
